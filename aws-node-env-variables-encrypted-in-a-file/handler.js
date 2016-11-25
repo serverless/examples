ed193a687721bcbd1b23d103af1627cf9d7ef370
Nik Graf <nik@nikgraf.com>
2016-11-25 16:18:03 +0100

    add example with encrypted envariables in a file

---
+++ aws-node-env-variables-encrypted-in-a-file/handler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* eslint-disable no-console */
+
+module.exports.resetPassword = (event, context, callback) => {
+  console.log('SESSION_KEY: ', process.env.SESSION_KEY);
+
+  // Authenticate the user session
+
+  console.log('EMAIL_SERVICE_API_KEY: ', process.env.EMAIL_SERVICE_API_KEY);
+
+  // The email service api key would be used to send a reset password email.
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Password sent.',
+    }),
+  };
+
+  callback(null, response);
+};
