04c3684e37009c64810f16f0bea67d443977a094
nielsla95 <nielsla95@gmail.com>
2016-12-05 23:39:01 +0100

    Authorizer

--- aws-auth0-api-gateway/handler.js
+++ aws-auth0-api-gateway/handler.js
@@ -3,6 +3,44 @@ const jwt = require('jsonwebtoken');
 const AUTH0_CLIENT_ID = 'your-auth0-client-id-here';
 const AUTH0_CLIENT_SECRET = 'your-auth0-client-secret-here';
 
+// Policy helper function
+const generatePolicy = function(principalId, effect, resource) {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+// Authorizer
+module.exports.auth = (event, context, cb) => {
+  if (event.authorizationToken) {
+    // remove "bearer " from token
+    const token = event.authorizationToken.substring(7);
+    const options = {
+      audience: AUTH0_CLIENT_ID,
+    };
+    jwt.verify(token, new Buffer(AUTH0_CLIENT_SECRET, 'base64'), options, (err, decoded) => {
+      if (err) {
+        cb('Unauthorized');
+      } else {
+        cb(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+      }
+    });
+  } else {
+    cb('Unauthorized');
+  }
+};
+
 // Public API
 module.exports.publicEndpoint = (event, context, cb) => {
   cb(null, { message: 'Welcome to our Public API!' });
@@ -10,20 +48,5 @@ module.exports.publicEndpoint = (event, context, cb) => {
 
 // Private API
 module.exports.privateEndpoint = (event, context, cb) => {
-  if (event.headers.Authorization) {
-    // remove "bearer " from token
-    const token = event.headers.Authorization.substring(7);
-    const options = {
-      audience: AUTH0_CLIENT_ID,
-    };
-    jwt.verify(token, new Buffer(AUTH0_CLIENT_SECRET, 'base64'), options, (err, decoded) => {
-      if (err) {
-        cb(null, { message: 'Access denied', token: err });
-      } else {
-        cb(null, { message: 'Access granted.', token: decoded });
-      }
-    });
-  } else {
-    cb(null, { message: 'You must be logged in to access this route! :(' });
-  }
+  cb(null, { message: 'Only logged in users can see this' });
 };
