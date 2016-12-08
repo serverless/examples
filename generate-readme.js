f2438db847b1fdc5762b7996bfe9329294775c56
davidwells <david@inboundnow.com>
2016-12-08 13:20:53 -0800

    update readme and readme-generator

--- generate-readme.js
+++ generate-readme.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 const markdownMagic = require('markdown-magic'); // eslint-disable-line
 const globby = require('markdown-magic').globby; // eslint-disable-line
 
@@ -9,6 +10,31 @@ const toTitleCase = (str) => { // eslint-disable-line
 
 const formatPluginName = (string) => { // eslint-disable-line
   return toTitleCase(string.replace(/-/g, ' '));
+};
+
+const username = (repo) => {
+  if (!repo) {
+    return null;
+  }
+
+  const o = url.parse(repo);
+  var urlPath = o.path; // eslint-disable-line
+
+  if (urlPath.length && urlPath.charAt(0) === '/') {
+    urlPath = urlPath.slice(1);
+  }
+
+  urlPath = urlPath.split('/')[0];
+  return urlPath;
+};
+
+const getRuntime = (dirname) => {
+  if (dirname.match(/node/)) {
+    return 'nodeJS';
+  } else if (dirname.match(/python/)) {
+    return 'python';
+  }
+  return 'nodeJS';
 };
 
 const config = {
@@ -22,14 +48,16 @@ const config = {
     SERVERLESS_EXAMPLE_TABLE() {
       const examples = globby.sync(['**/package.json', '!node_modules/**/package.json', '!**/node_modules/**/package.json', '!package.json']);
       // Make table header
-      let md = '| Example | description  |\n';
+      let md = '| Example | Runtime  |\n';
       md += '|:--------------------------- |:-----|\n';
       examples.forEach((example) => {
         const data = JSON.parse(fs.readFileSync(example, 'utf8'));
         const dirname = path.dirname(example);
-        const url = `https://github.com/serverless/examples/tree/master/${dirname}`;
+        const exampleUrl = `https://github.com/serverless/examples/tree/master/${dirname}`;
+        const runtime = getRuntime(dirname);
+        const description = (data.description) ? `<br/> ${data.description}` : '';
         // add table rows
-        md += `| [${formatPluginName(data.name)}](${url}) | ${data.description} |\n`;
+        md += `| [${formatPluginName(data.name)}](${exampleUrl}) ${description} | ${runtime} |\n`;
       });
 
       return md;
@@ -44,12 +72,15 @@ const config = {
       const exampleFile = path.join(__dirname, 'community-examples.json');
       const examples = JSON.parse(fs.readFileSync(exampleFile, 'utf8'));
       // Make table header
-      let md = '| Example | description  |\n';
-      md += '|:--------------------------- |:-----|\n';
+      let md = '| Example | Author |\n';
+      md += '|:-------|:------:|\n';
       // Sort alphabetically
       examples.sort((a, b) => a.name < b.name ? -1 : 1).forEach((data) => { // eslint-disable-line
         // add table rows
-        md += `| [${formatPluginName(data.name)}](${data.githubUrl}) | ${data.description} |\n`;
+        const userName = username(data.githubUrl);
+        const profileURL = `http://github.com/${userName}`;
+        md += `| **[${formatPluginName(data.name)}](${data.githubUrl})** <br/>`;
+        md += ` ${data.description} | [${userName}](${profileURL}) | \n`;
       });
       return md.replace(/^\s+|\s+$/g, '');
     },
