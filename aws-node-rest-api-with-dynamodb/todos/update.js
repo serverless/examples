8cb5e721dbc8c533bb624e32c24f8c93fbd2b065
Velu Mani <velulev@gmail.com>
2016-12-30 22:43:45 +0000

    Revert "change from put to update"
    This reverts commit ad82454d873f5d2c6bfdd6865616b41078489e58.

--- aws-node-rest-api-with-dynamodb/todos/update.js
+++ aws-node-rest-api-with-dynamodb/todos/update.js
@@ -9,34 +9,24 @@ module.exports.update = (event, context, callback) => {
   const data = JSON.parse(event.body);
 
   // validation
-  if (typeof data.text !== 'string' || typeof data.checked !== 'boolean') {
+  if (typeof data.text !== 'string' && typeof data.checked !== 'boolean') {
     console.error('Validation Failed'); // eslint-disable-line no-console
     callback(new Error('Couldn\'t create the todo item.'));
     return;
   }
 
-const params = {
+  const params = {
     TableName: 'todos',
-	Key: {
-		id: event.pathParameters.id
-	},
-	ExpressionAttributeNames: {
-		"#c1": "createdAt",
-		"#c2": "updatedAt",
-		"#c3": "text",
-		"#c4": "checked"
-	},
-	ExpressionAttributeValues: {
-		":d2": timestamp,
-		":d3": data.text,
-		":d4": data.checked
-	},
-	UpdateExpression: "SET #c1 = if_not_exists(#c1, :d2), #c2 = :d2, #c3 = :d3, #c4 = :d4",
-	ReturnValues: "ALL_NEW"
-};
+    Item: {
+      id: event.pathParameters.id,
+      text: data.text,
+      checked: data.checked,
+      updatedAt: timestamp,
+    },
+  };
 
   // update the todo in the database
-  dynamoDb.update(params, (error, result) => {
+  dynamoDb.put(params, (error, result) => {
     // handle potential errors
     if (error) {
       console.error(error); // eslint-disable-line no-console
@@ -47,7 +37,7 @@ const params = {
     // create a resonse
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Attributes),
+      body: JSON.stringify(result.Item),
     };
     callback(null, response);
   });
