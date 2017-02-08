7950d3783d4fafaf4fc9af57a7b7804d775bc0c2
Christoph Gysin <christoph.gysin@gmail.com>
2017-02-08 16:44:56 +0200

    add aws-python-rest-api-with-dynamodb
    Adds a python REST example using DynamoDB.
    
    fixes #13

---
+++ aws-python-rest-api-with-dynamodb/todos/decimalencoder.py
@@ -0,0 +1,10 @@
+import decimal
+import json
+
+
+# This is a workaround for: http://bugs.python.org/issue16535
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal):
+            return int(obj)
+        return super(DecimalEncoder, self).default(obj)
