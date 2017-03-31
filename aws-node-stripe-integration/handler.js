388c27c8564fccb2cdcea8cf43609dfe42b36d49
wokamoto <wokamoto1973@gmail.com>
2017-03-31 21:52:39 +0900

    fixed eslint errors

--- aws-node-stripe-integration/handler.js
+++ aws-node-stripe-integration/handler.js
@@ -1,59 +1,46 @@
 'use strict';
 
+const ConfigFile = require('config'); // eslint-disable-line
+
 module.exports.incoming = (event, context, callback) => {
-  const stage =
+  const requestContextStage =
     event.requestContext
     ? event.requestContext.stage
     : 'test';
-
-  let ConfigFile = require('config');
-	let async = require('async');
-
-  let stripe_api_key    =
-    stage == 'test'
+  const stripeApiKey =
+    requestContextStage === 'test'
     ? ConfigFile.stripe.test_sk
     : ConfigFile.stripe.live_sk;
-  let stripe = require('stripe')(stripe_api_key);
+  const stripe = require('stripe')(stripeApiKey); // eslint-disable-line
 
-  console.log('Event: %j', event);
-  async.waterfall([
-  function(nextProcess) {
-    let stripe_event;
-    try {
-      stripe_event = JSON.parse(event.body); // https://stripe.com/docs/api#event_object
-      console.log('Incoming Data: %j', stripe_event);
-      nextProcess(null, stripe_event);
-    } catch(err){
-      nextProcess(err);
-    }
-  },
-  function(stripe_event, nextProcess) {
+  try {
+    // Parse Stripe Event
+    const jsonData = JSON.parse(event.body); // https://stripe.com/docs/api#event_object
+
     // Verify the event by fetching it from Stripe
-    stripe.events.retrieve(stripe_event.id, nextProcess);
-  },
-  function(stripe_event, nextProcess) {
-    // Branch by the event type
-    let event_type = stripe_event.type ? stripe_event.type : '';
-    console.log('Event: ' + event_type);
+    console.log("Stripe Event: %j", jsonData); // eslint-disable-line
+    stripe.events.retrieve(jsonData.id, (err, stripeEvent) => {
+      const eventType = stripeEvent.type ? stripeEvent.type : '';
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Stripe webhook incoming!',
+          stage: requestContextStage,
+        }),
+      };
+      console.log("Event Type: %j", eventType); // eslint-disable-line
 
-    switch(event_type) {
-      case 'invoice.created':
-        nextProcess(null, stripe_event);
-        break;
-      default:
-        nextProcess(null, stripe_event);
-        break;
-    }
-  }],
-  function(err, result) {
-    let response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'Stripe webhook incoming!',
-        stage: stage
-      }),
-    };
-    if (err) console.log('Error: %j', err);
-    callback(null, response);
-  });
+      // Branch by event type
+      switch (eventType) {
+        case 'invoice.created':
+          // invoice.created event
+          break;
+        default:
+          break;
+      }
+      callback(null, response);
+    });
+  } catch (err) {
+    context.fail(err);
+  }
 };
