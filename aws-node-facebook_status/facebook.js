5744edbc003d8840d323f9a7309894a16446f482
Mihai Serban <mihaiserban2@gmail.com>
2017-05-09 18:54:44 +0300

    fix linter errors
    fix linter errors

--- aws-node-facebook_status/facebook.js
+++ aws-node-facebook_status/facebook.js
@@ -3,24 +3,22 @@
 // ES5
 const FB = require('fb');
 
-module.exports = function () {
-  this.post = function (data, token, callback) {
-    FB.setAccessToken(token);
+module.exports.post = function post(data, token, callback) {
+  FB.setAccessToken(token);
 
-    const parameters = {};
-    if (Object.prototype.hasOwnProperty.call(data, 'message')) {
-      parameters.message = data.message;
-    }
-    if (Object.prototype.hasOwnProperty.call(data, 'link')) {
-      parameters.link = data.link;
-    }
+  const parameters = {};
+  if (Object.prototype.hasOwnProperty.call(data, 'message')) {
+    parameters.message = data.message;
+  }
+  if (Object.prototype.hasOwnProperty.call(data, 'link')) {
+    parameters.link = data.link;
+  }
 
-    FB.api('me/feed', 'post', parameters, (res) => {
-      if (!res || res.error) {
-        callback(res.error, null);
-      } else {
-        callback(null, res.id);
-      }
-    });
-  };
+  FB.api('me/feed', 'post', parameters, (res) => {
+    if (!res || res.error) {
+      callback(res.error, null);
+    } else {
+      callback(null, res.id);
+    }
+  });
 };
--- aws-node-facebook_status/handler.js
+++ aws-node-facebook_status/handler.js
@@ -3,8 +3,7 @@
 const Facebook = require('./facebook.js');
 
 module.exports.postFb = (event, context, callback) => {
-  const facebook = new Facebook();
-  facebook.post(event.data, event.accessToken, (error, statusId) => {
+  Facebook.post(event.data, event.accessToken, (error, statusId) => {
     if (error) {
       callback(event.data); // error, send back the event
     } else {
