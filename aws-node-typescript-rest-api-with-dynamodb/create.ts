a1d8d66a14cf09e6fae4171cbf9142692a2f4e2c
Nikos Katsikanis <nkatsikanis@gmail.com>
2017-07-28 20:29:29 +0100

    👕 Linting for typescript

--- aws-node-typescript-rest-api-with-dynamodb/create.ts
+++ aws-node-typescript-rest-api-with-dynamodb/create.ts
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 import uuid from 'uuid'
 import { DynamoDB } from 'aws-sdk'
 
-const dynamoDb = new DynamoDB.DocumentClient();
+const dynamoDb = new DynamoDB.DocumentClient()
 
 module.exports.create = (event, context, callback) => {
-  const timestamp = new Date().getTime();
-  const data = JSON.parse(event.body);
+  const timestamp = new Date().getTime()
+  const data = JSON.parse(event.body)
   if (typeof data.text !== 'string') {
-    console.error('Validation Failed');
-    callback(new Error('Couldn\'t create the todo item.'));
-    return;
+    console.error('Validation Failed')
+    callback(new Error('Couldn\'t create the todo item.'))
+    return
   }
 
   const params = {
@@ -21,24 +21,24 @@ module.exports.create = (event, context, callback) => {
       text: data.text,
       checked: false,
       createdAt: timestamp,
-      updatedAt: timestamp,
-    },
-  };
+      updatedAt: timestamp
+    }
+  }
 
   // write the todo to the database
   dynamoDb.put(params, (error, result) => {
     // handle potential errors
     if (error) {
-      console.error(error);
-      callback(new Error('Couldn\'t create the todo item.'));
-      return;
+      console.error(error)
+      callback(new Error('Couldn\'t create the todo item.'))
+      return
     }
 
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item),
-    };
-    callback(null, response);
-  });
-};
+      body: JSON.stringify(result.Item)
+    }
+    callback(null, response)
+  })
+}
