512acb105359b75e0ddc1ae59c585d76d50c08f0
Assaf Gannon <assaf.gannon@gmai.com>
2018-03-17 12:18:21 +0200

    updated auth0 lock version and fixed auth function

--- aws-node-auth0-custom-authorizers-api/handler.js
+++ aws-node-auth0-custom-authorizers-api/handler.js
@@ -1,92 +1,90 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 // Set in `enviroment` of serverless.yml
-const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID
-const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const AUTH0_CLIENT_PUBLIC_KEY = process.env.AUTH0_CLIENT_PUBLIC_KEY;
 
 // Policy helper function
 const generatePolicy = (principalId, effect, resource) => {
-  const authResponse = {}
-  authResponse.principalId = principalId
+  const authResponse = {};
+  authResponse.principalId = principalId;
   if (effect && resource) {
-    const policyDocument = {}
-    policyDocument.Version = '2012-10-17'
-    policyDocument.Statement = []
-    const statementOne = {}
-    statementOne.Action = 'execute-api:Invoke'
-    statementOne.Effect = effect
-    statementOne.Resource = resource
-    policyDocument.Statement[0] = statementOne
-    authResponse.policyDocument = policyDocument
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
   }
-  return authResponse
-}
+  return authResponse;
+};
 
 // Reusable Authorizer function, set on `authorizer` field in serverless.yml
 module.exports.auth = (event, context, callback) => {
-  console.log('event', event)
+  console.log('event', event);
   if (!event.authorizationToken) {
-    return callback('Unauthorized')
+    return callback('Unauthorized');
   }
 
-  const tokenParts = event.authorizationToken.split(' ')
-  const tokenValue = tokenParts[1]
+  const tokenParts = event.authorizationToken.split(' ');
+  const tokenValue = tokenParts[1];
 
   if (!(tokenParts[0].toLowerCase() === 'bearer' && tokenValue)) {
     // no auth token!
-    return callback('Unauthorized')
+    return callback('Unauthorized');
   }
   const options = {
     audience: AUTH0_CLIENT_ID,
-  }
-  // decode base64 secret. ref: http://bit.ly/2hA6CrO
-  const secret = new Buffer.from(AUTH0_CLIENT_SECRET, 'base64')
+  };
+
   try {
-    jwt.verify(tokenValue, secret, options, (verifyError, decoded) => {
+    jwt.verify(tokenValue, AUTH0_CLIENT_PUBLIC_KEY, options, (verifyError, decoded) => {
       if (verifyError) {
-        console.log('verifyError', verifyError)
+        console.log('verifyError', verifyError);
         // 401 Unauthorized
-        console.log(`Token invalid. ${verifyError}`)
-        return callback('Unauthorized')
+        console.log(`Token invalid. ${verifyError}`);
+        return callback('Unauthorized');
       }
       // is custom authorizer function
-      console.log('valid from customAuthorizer', decoded)
-      return callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn))
-    })
-   } catch (err) {
-    console.log('catch error. Invalid token', err)
-    return callback('Unauthorized')
+      console.log('valid from customAuthorizer', decoded);
+      return callback(null, generatePolicy(decoded.sub, 'Allow', event.methodArn));
+    });
+  } catch (err) {
+    console.log('catch error. Invalid token', err);
+    return callback('Unauthorized');
   }
-}
+
+  // if for any reason you get here...
+  return callback('Unauthorized');
+};
 
 // Public API
-module.exports.publicEndpoint = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
+module.exports.publicEndpoint = (event, context, callback) => callback(null, {
+  statusCode: 200,
+  headers: {
       /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
+    'Access-Control-Allow-Origin': '*',
       /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  })
-}
+    'Access-Control-Allow-Credentials': true,
+  },
+  body: JSON.stringify({
+    message: 'Hi ⊂◉‿◉つ from Public API',
+  }),
+});
 
 // Private API
-module.exports.privateEndpoint = (event, context, callback) => {
-  return callback(null, {
-    statusCode: 200,
-    headers: {
+module.exports.privateEndpoint = (event, context, callback) => callback(null, {
+  statusCode: 200,
+  headers: {
       /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
+    'Access-Control-Allow-Origin': '*',
       /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true
-    },
-    body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Private API. Only logged in users can see this',
-    }),
-  })
-}
+    'Access-Control-Allow-Credentials': true,
+  },
+  body: JSON.stringify({
+    message: 'Hi ⊂◉‿◉つ from Private API. Only logged in users can see this',
+  }),
+});
