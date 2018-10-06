e8e730c2949be374670a7ca2bc3222d2d0b4ae6f
Jiyeon Seo <jiyeonseo@fb.com>
2018-10-06 12:01:04 +0900

    fix params as string

--- azure-node-line-bot/handler.js
+++ azure-node-line-bot/handler.js
@@ -6,8 +6,8 @@ const line = require('@line/bot-sdk');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: "CHANNEL_ACCESS_TOKEN",
+  channelSecret: "CHANNEL_SECRET",
 };
 
 const client = new line.Client(config);
