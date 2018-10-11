c004a93a824d2eb531aaffd3983bd253726c314a
Duong Tran <duong.tran@gridstone.com.au>
2018-10-11 14:36:50 +1100

    style: update linting style

--- aws-node-simple-transcribe-s3/handler.js
+++ aws-node-simple-transcribe-s3/handler.js
@@ -1,16 +1,17 @@
 'use strict';
 
 const awsSdk = require('aws-sdk');
+
 const transcribeService = new awsSdk.TranscribeService();
 
 module.exports.transcribe = (event, context, callback) => {
   const records = event.Records;
 
-  const transcribingPromises = records.map(record => {
+  const transcribingPromises = records.map((record) => {
     const recordUrl = [
       'https://s3.amazonaws.com',
       process.env.S3_AUDIO_BUCKET,
-      record.s3.object.key
+      record.s3.object.key,
     ].join('/');
 
     const TranscriptionJobName = record.s3.object.key;
@@ -21,7 +22,7 @@ module.exports.transcribe = (event, context, callback) => {
       MediaFormat: 'wav',
       TranscriptionJobName,
       MediaSampleRateHertz: 8000, // normally 8000 if you are using wav file
-      OutputBucketName: process.env.S3_TRANSCRIPTION_BUCKET
+      OutputBucketName: process.env.S3_TRANSCRIPTION_BUCKET,
     }).promise();
   });
 
@@ -29,5 +30,5 @@ module.exports.transcribe = (event, context, callback) => {
     .then(() => {
       callback(null, { message: 'Start transcription job successfully' });
     })
-    .catch(err => callback(err, { message: 'Error start transcription job'}))
+    .catch(err => callback(err, { message: 'Error start transcription job' }));
 };
