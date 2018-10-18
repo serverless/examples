3cbe2d1dea1d16d306e22e41d0a956ff1b58f2bb
adnanrahic <ado.raha198@gmail.com>
2018-10-18 17:35:02 +0200

    (lint) fixed errors

--- aws-node-vue-nuxt-ssr/index.js
+++ aws-node-vue-nuxt-ssr/index.js
@@ -1,12 +1,7 @@
-const sls = require('serverless-http')
-const binaryMimeTypes = require('./binaryMimeTypes')
+const sls = require('serverless-http');
+const binaryMimeTypes = require('./binaryMimeTypes');
+const nuxt = require('./nuxt');
 
-const api = require('./api')
-module.exports.api = sls(api, {
-  binary: binaryMimeTypes
-})
-
-const nuxt = require('./nuxt')
 module.exports.nuxt = sls(nuxt, {
-  binary: binaryMimeTypes
-})
+  binary: binaryMimeTypes,
+});
