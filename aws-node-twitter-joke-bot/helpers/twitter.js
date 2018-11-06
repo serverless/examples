31986d2d4b2320454af9034180b79b53e0dd7af8
Craig Sweaton <craig.sweaton@financial-cloud.com>
2018-11-06 22:16:57 +0000

    Lint: Fixed linting problems

--- aws-node-twitter-joke-bot/helpers/twitter.js
+++ aws-node-twitter-joke-bot/helpers/twitter.js
@@ -1,20 +1,18 @@
 const Twitter = require('twitter');
 
 const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-const tweetJoke = (joke) => {
-    return new Promise((resolve, reject) => {
-        client.post('statuses/update', { status: joke })
-            .then((tweet) => resolve(tweet))
-            .catch((error) => reject(error));
-    });
-};
+const tweetJoke = joke => new Promise((resolve, reject) => {
+  client.post('statuses/update', { status: joke })
+            .then(tweet => resolve(tweet))
+            .catch(error => reject(error));
+});
 
 module.exports = {
-    tweetJoke,
+  tweetJoke,
 };
