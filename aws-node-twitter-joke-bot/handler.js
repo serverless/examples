59ed60e7e889edba777f35f5a1a5bc96c0b6f02b
Craig Sweaton <craig.sweaton@financial-cloud.com>
2018-11-06 21:51:47 +0000

    Example: Added aws-node-twitter-joke-bot to examples

---
+++ aws-node-twitter-joke-bot/handler.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { getDadJoke } = require('./helpers/jokes');
+const { tweetJoke } = require('./helpers/twitter');
+
+module.exports.bot = (event, context, callback) => {
+  // Call jokes api to get a joke
+  getDadJoke()
+    .then((json) => {
+      // Check for a successful response
+      // Bail if not!
+      if (json.status !== 200) {
+        return callback(null, { statusCode: json.status, body: JSON.stringify({ error: 'Could not fetch a joke' }) });
+      }
+
+      // Get the joke text
+      const { joke } = json;
+      console.log(`JOKE API RESPONSE ==> ${joke}`);
+
+      // Set up the twitter module
+      // Tweet the joke
+      tweetJoke(joke)
+        .then((response) => {
+          console.log(JSON.stringify(response));
+          return callback(null, { statusCode: json.status, body: JSON.stringify({ message: response }) })
+        })
+        .catch((error) => {
+          console.error(error);
+          return callback(null, { statusCode: 500, body: JSON.stringify({ error }) })
+        });
+    })
+    .catch((error) => {
+      console.error(error);
+      return callback(null, { statusCode: 500, body: JSON.stringify({ error }) })
+    });
+};
---
+++ aws-node-twitter-joke-bot/helpers/jokes.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+
+const options = {
+    headers: {
+        Accept: 'application/json',
+        'User-Agent': 'My Library (YOUR LIBRARY URL HERE)',
+    },
+};
+
+const getDadJoke = () => {
+    return new Promise((resolve, reject) => {
+        fetch(process.env.JOKES_API_URL, options)
+            .then((response) => response.json())
+            .then((json) => resolve(json))
+            .catch((error) => reject(error));
+    });
+};
+
+module.exports = {
+    getDadJoke,
+};
