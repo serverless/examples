7408b84559795af19388775088cd9788439129cd
Sebastian Borza <sebito91@gmail.com>
2018-11-15 16:55:04 -0600

    initial commit for awskinesis stream to elasticsearch

---
+++ aws-golang-stream-kinesis-to-elasticsearch/elastic/elastic.go
@@ -0,0 +1,44 @@
+package elastic
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/olivere/elastic"
+)
+
+// Elastic is our default struct for connecting through to elasticsearch
+type Elastic struct {
+	Client *elastic.Client
+}
+
+const host = "http://es.signalmalt.com"
+
+// NewClient instantiates a connection to elasticsearch for direct push
+func NewClient() (*Elastic, error) {
+	client, err := elastic.NewClient(
+		elastic.SetURL(host),
+		elastic.SetScheme("http"),
+		elastic.SetSniff(false),
+		elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),
+		elastic.SetInfoLog(log.New(os.Stdout, "", log.LstdFlags)),
+	)
+	return &Elastic{client}, err
+}
+
+// PushRecords sends a particular batch of docs over to elasticsearch endpoint
+func (e *Elastic) PushRecords(data []events.KinesisEventRecord) error {
+	for _, x := range data {
+		_, err := e.Client.Index().
+			Index("sebtest-apm").
+			Type("doc").
+			BodyJson(x.Kinesis.Data).
+			Do(context.Background())
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ aws-golang-stream-kinesis-to-elasticsearch/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	elastic "github.com/serverless/examples/aws-golang-stream-kinesis-to-elasticsearch/elastic"
+)
+
+func handler(ctx context.Context, event events.KinesisEvent) error {
+	client, err := elastic.NewClient()
+	if err != nil {
+		return err
+	}
+
+	return client.PushRecords(event.Records)
+}
+
+func main() {
+	lambda.Start(handler)
+}
