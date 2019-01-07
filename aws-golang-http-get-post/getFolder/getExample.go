ae0274ebc488befa1d0fd6ee20d8b4666222da2a
Pramono <pramono_winata@yahoo.com>
2019-01-07 22:22:14 +0700

    Added examples for golang http get and post with AWS lambda struct Proxy

---
+++ aws-golang-http-get-post/getFolder/getExample.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Handler function Using AWS Lambda Proxy Request
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	//Get the path parameter that was sent
+	name := request.PathParameters["name"]
+
+	//Generate message that want to be sent as body
+	message := fmt.Sprintf(" { \"Message\" : \"Hello %s \" } ", name)
+
+	//Returning response with AWS Lambda Proxy Response
+	return events.APIGatewayProxyResponse{Body: message, StatusCode: 200}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
