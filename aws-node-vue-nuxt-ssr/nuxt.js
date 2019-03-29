2a537e8bd34575928030a0a35282e19131221a7a
Alexander Lichter <manniL@gmx.net>
2019-03-29 12:19:54 +0100

    fix: await nuxt ready state

--- aws-node-vue-nuxt-ssr/nuxt.js
+++ aws-node-vue-nuxt-ssr/nuxt.js
@@ -3,9 +3,11 @@ const { Nuxt } = require('nuxt');
 const path = require('path');
 const config = require('./nuxt.config.js');
 
-const app = express();
-const nuxt = new Nuxt(config);
-app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
-app.use(nuxt.render);
-
-module.exports = app;
+module.exports = async function() {
+  const app = express()
+  const nuxt = new Nuxt(config)
+  await nuxt.ready()
+  app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')))
+  app.use(nuxt.render)
+  return app
+}
