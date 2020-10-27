869c9b7d8b290009bf25d3e4bde3df12f6db1f66
Paulo Rodrigues <pcqrodrigues@gmail.com>
2020-10-27 19:51:20 -0300

    feat(examples): Added new example of node

---
+++ aws-node-rest-api-babel-webpack/webpack.config.js
@@ -0,0 +1,26 @@
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  devtool: 'source-map',
+  externals: [nodeExternals()],
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+    minimize: false,
+  },
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
