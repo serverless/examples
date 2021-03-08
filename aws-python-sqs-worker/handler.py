fdd302130f9d3369d5530a53476902650625164a
Piotr Grzesik <pj.grzesik@gmail.com>
2021-03-08 13:25:45 +0100

    feat: Add `aws-python-sqs-worker` example

---
+++ aws-python-sqs-worker/handler.py
@@ -0,0 +1,44 @@
+import json
+import logging
+import os
+
+import boto3
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+QUEUE_URL = os.getenv('QUEUE_URL')
+SQS = boto3.client('sqs')
+
+
+def producer(event, context):
+    status_code = 200
+    message = ''
+
+    if not event['body']:
+        return {'statusCode': 400, 'body': json.dumps({'message': 'No body was found'})}
+
+    try:
+        message_attrs = {
+            'AttributeName': {'StringValue': 'AttributeValue', 'DataType': 'String'}
+        }
+        SQS.send_message(
+            QueueUrl=QUEUE_URL,
+            MessageBody=event['body'],
+            MessageAttributes=message_attrs,
+        )
+        message = 'Message accepted!'
+    except Exception as e:
+        logger.exception('Sending message to SQS queue failed!')
+        message = str(e)
+        status_code = 500
+
+    return {'statusCode': status_code, 'body': json.dumps({'message': message})}
+
+
+def consumer(event, context):
+    for record in event['Records']:
+        logger.info(f'Message body: {record["body"]}')
+        logger.info(
+            f'Message attribute: {record["messageAttributes"]["AttributeName"]["stringValue"]}'
+        )
