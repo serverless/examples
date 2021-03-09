04dd3b304e6cdb451b97d838046fa2f33a529989
Piotr Grzesik <pj.grzesik@gmail.com>
2021-03-09 13:10:38 +0100

    feat: Add `aws-python-flask-dynamodb-api` template

---
+++ aws-python-flask-dynamodb-api/app.py
@@ -0,0 +1,50 @@
+import os
+
+import boto3
+from flask import Flask, jsonify, make_response, request
+
+app = Flask(__name__)
+
+
+dynamodb_client = boto3.client('dynamodb')
+
+if os.environ.get('IS_OFFLINE'):
+    dynamodb_client = boto3.client(
+        'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
+    )
+
+
+USERS_TABLE = os.environ['USERS_TABLE']
+
+
+@app.route('/users/<string:user_id>')
+def get_user(user_id):
+    result = dynamodb_client.get_item(
+        TableName=USERS_TABLE, Key={'userId': {'S': user_id}}
+    )
+    item = result.get('Item')
+    if not item:
+        return jsonify({'error': 'Could not find user with provided "userId"'}), 404
+
+    return jsonify(
+        {'userId': item.get('userId').get('S'), 'name': item.get('name').get('S')}
+    )
+
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    user_id = request.json.get('userId')
+    name = request.json.get('name')
+    if not user_id or not name:
+        return jsonify({'error': 'Please provide both "userId" and "name"'}), 400
+
+    dynamodb_client.put_item(
+        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'name': {'S': name}}
+    )
+
+    return jsonify({'userId': user_id, 'name': name})
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 404)
