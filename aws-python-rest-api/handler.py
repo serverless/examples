4655821f26b8d8e870dcb5baba6c139518c9c7c3
Piotr Grzesik <pj.grzesik@gmail.com>
2021-03-09 15:19:50 +0100

    feat: Refresh `aws-python-rest-api` template

--- aws-python-rest-api/handler.py
+++ aws-python-rest-api/handler.py
@@ -3,14 +3,11 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "message": "Go Serverless v2.0! Your function executed successfully!",
+        "input": event,
     }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
 
