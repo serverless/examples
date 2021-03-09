44e691b5d761489d210d965b81b149104cc81caf
Piotr Grzesik <pj.grzesik@gmail.com>
2021-03-09 15:07:26 +0100

    feat: Refresh `aws-node-rest-api` template

--- aws-node-rest-api/handler.js
+++ aws-node-rest-api/handler.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async event => {
+module.exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v2.0! Your function executed successfully!",
         input: event,
       },
       null,
