d488da0738259b63bcb1603bcc7f83a2d84b7ce7
arthur-cheung <arthur.cheung.au@gmail.com>
2021-08-26 19:05:06 +1000

    fix: Added `shell: true` option to `spawnSync` (#641)

--- aws-node-single-page-app-via-cloudfront/serverless-single-page-app-plugin/index.js
+++ aws-node-single-page-app-via-cloudfront/serverless-single-page-app-plugin/index.js
@@ -44,7 +44,7 @@ class ServerlessPlugin {
     if (this.serverless.variables.service.provider.profile) {
       command = `${command} --profile ${this.serverless.variables.service.provider.profile}`;
     }
-    const result = spawnSync(command, args);
+    const result = spawnSync(command, args, { shell: true });
     const stdout = result.stdout.toString();
     const sterr = result.stderr.toString();
     if (stdout) {
