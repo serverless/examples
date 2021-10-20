ade0fd6175c77a0aeaf3f3c4b959ac58675b5a76
Saurabh <saurabh@codeword.tech>
2021-10-20 12:21:39 +0530

    fix: spelling error

--- aws-node-sqs-worker/handler.js
+++ aws-node-sqs-worker/handler.js
@@ -48,7 +48,7 @@ const consumer = async (event) => {
   for (const record of event.Records) {
     const messageAttributes = record.messageAttributes;
     console.log(
-      "Message Attributte: ",
+      "Message Attribute: ",
       messageAttributes.AttributeName.stringValue
     );
     console.log("Message Body: ", record.body);
