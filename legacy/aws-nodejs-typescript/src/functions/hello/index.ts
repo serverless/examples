cbc7615fb29c2b746af2ea53a41ab6cd01d275bc
Piotr Grzesik <pj.grzesik@gmail.com>
2022-01-27 21:31:02 +0100

    feat: Add legacy templates

---
+++ legacy/aws-nodejs-typescript/src/functions/hello/index.ts
@@ -0,0 +1,19 @@
+import schema from './schema';
+import { handlerPath } from '@libs/handler-resolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      http: {
+        method: 'post',
+        path: 'hello',
+        request: {
+          schemas: {
+            'application/json': schema,
+          },
+        },
+      },
+    },
+  ],
+};
