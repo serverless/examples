a5a2cd79d6066fda7429c891dfaf40bd9496bdeb
Stuart lang <stuart.b.lang@gmail.com>
2022-02-27 13:19:23 +0000

    Modernise C#

--- legacy/aws-csharp/Handler.cs
+++ legacy/aws-csharp/Handler.cs
@@ -1,31 +1,29 @@
-using Amazon.Lambda.Core;
-
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-namespace AwsDotnetCsharp
+
+namespace AwsDotnetCsharp;
+public class Handler
 {
-    public class Handler
+    public Response Hello(Request request)
     {
-       public Response Hello(Request request)
-       {
-           return new Response("Go Serverless v1.0! Your function executed successfully!", request);
-       }
+        return new Response("Go Serverless v1.0! Your function executed successfully!", request);
     }
+}
 
-    public class Response
-    {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+public class Response
+{
+  public string Message {get; set;}
+  public Request Request {get; set;}
 
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
-    }
+  public Response(string message, Request request)
+  {
+    Message = message;
+    Request = request;
+  }
+}
 
-    public class Request
-    {
-      public string Key1 {get; set;}
-      public string Key2 {get; set;}
-      public string Key3 {get; set;}
-    }
+public class Request
+{
+  public string Key1 {get; set;}
+  public string Key2 {get; set;}
+  public string Key3 {get; set;}
 }
