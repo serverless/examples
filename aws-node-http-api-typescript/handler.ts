85a9de00f90465754e2f8c9744af028ffb0bc98d
Daniel Aniszkiewicz <daniel.aniszkiewicz@gmail.com>
2022-08-06 13:49:56 +0200

    [Feature] Improve the HTTP API typescript example

--- aws-node-http-api-typescript/handler.ts
+++ aws-node-http-api-typescript/handler.ts
@@ -1,19 +1,17 @@
-import { Handler } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export const hello: Handler = (event: any) => {
-  const response = {
+export const hello = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
+        message: "Go Serverless v3.0! Your function executed successfully!",
         input: event,
       },
       null,
       2
     ),
   };
-
-  return new Promise((resolve) => {
-    resolve(response)
-  })
-}
+};
