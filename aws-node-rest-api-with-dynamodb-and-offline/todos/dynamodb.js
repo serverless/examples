c9106c58fbd6fb2205aa94de433d1e047f5035e4
Richard Wright <richard.wright@imgarena.com>
2022-09-26 17:17:09 +0100

    feat: update serverless and code to fix dynamodb local connection issues

--- aws-node-rest-api-with-dynamodb-and-offline/todos/dynamodb.js
+++ aws-node-rest-api-with-dynamodb-and-offline/todos/dynamodb.js
@@ -9,6 +9,8 @@ if (process.env.IS_OFFLINE) {
   options = {
     region: 'localhost',
     endpoint: 'http://localhost:8000',
+    accessKeyId: 'DEFAULT_ACCESS_KEY',  // needed if you don't have aws credentials at all in env
+    secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
   };
 }
 
