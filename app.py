f26d8bcab7b2b581754bc9b862e4408a49744fec
Caleb M. Cook <calebmcook@icloud.com>
2023-01-16 21:20:03 -0700

    updates

--- app.py
+++ app.py
@@ -8,41 +8,25 @@ app = Flask(__name__)
 
 dynamodb_client = boto3.client('dynamodb')
 
-if os.environ.get('IS_OFFLINE'):
-    dynamodb_client = boto3.client(
-        'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
-    )
-
+# if os.environ.get('IS_OFFLINE'):
+#     dynamodb_client = boto3.client(
+#         'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
+#     )
 
 USERS_TABLE = os.environ['USERS_TABLE']
 
-
-@app.route('/users/<string:user_id>')
-def get_user(user_id):
-    result = dynamodb_client.get_item(
-        TableName=USERS_TABLE, Key={'userId': {'S': user_id}}
-    )
-    item = result.get('Item')
-    if not item:
-        return jsonify({'error': 'Could not find user with provided "userId"'}), 404
-
-    return jsonify(
-        {'userId': item.get('userId').get('S'), 'name': item.get('name').get('S')}
-    )
-
-
 @app.route('/users', methods=['POST'])
 def create_user():
-    user_id = request.json.get('userId')
-    name = request.json.get('name')
-    if not user_id or not name:
-        return jsonify({'error': 'Please provide both "userId" and "name"'}), 400
+    status = request.form['Body']
+    phone_number = request.form['From']
+    if not status or not phone_number:
+        return jsonify({'error': 'Please provide both "status" and "phone_number"'}), 400
 
     dynamodb_client.put_item(
-        TableName=USERS_TABLE, Item={'userId': {'S': user_id}, 'name': {'S': name}}
+        TableName=USERS_TABLE, Item={'status': {'S': status}, 'phone_number': {'S': phone_number}}
     )
 
-    return jsonify({'userId': user_id, 'name': name})
+    return jsonify({'status': status, 'phone_number': phone_number})
 
 
 @app.errorhandler(404)
