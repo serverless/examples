68f243168a46444b318c264ed9a63c4814a7e188
Caleb M. Cook <calebmcook@icloud.com>
2023-01-22 23:16:46 -0700

    updates

--- app.py
+++ app.py
@@ -1,32 +1,70 @@
 import os
-
 import boto3
-from flask import Flask, jsonify, make_response, request
+from flask import Flask, jsonify, make_response, request, abort
+from twilio.request_validator import RequestValidator
+from twilio.twiml.messaging_response import MessagingResponse
+import logging
 
+
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-
-dynamodb_client = boto3.client('dynamodb')
-
+## local debug
 # if os.environ.get('IS_OFFLINE'):
 #     dynamodb_client = boto3.client(
 #         'dynamodb', region_name='localhost', endpoint_url='http://localhost:8000'
 #     )
 
 USERS_TABLE = os.environ['USERS_TABLE']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+
+dynamodb_client = boto3.client('dynamodb')
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    status = request.form['Body']
+    #validate that the request originates from Twilio
+    validator = RequestValidator(TWILIO_AUTH_TOKEN)
+    if not validator.validate(request.url, request.form, request.headers.get('X-Twilio-Signature')):
+        abort(400)
+
+    #pull data from request
+    status = str.upper(request.form['Body'])
     phone_number = request.form['From']
-    if not status or not phone_number:
-        return jsonify({'error': 'Please provide both "status" and "phone_number"'}), 400
 
-    dynamodb_client.put_item(
-        TableName=USERS_TABLE, Item={'status': {'S': status}, 'phone_number': {'S': phone_number}}
-    )
+    #response logic
+    if status == 'DAILY-SMS':
+        msg = 'Thank you for your interest in the ISI daily bible verse service! You are now subscribed to the SMS sent at 7am AZ time. Text "STOP-SERVICES" any time to cancel your subscription.'
+    elif status == 'STOP-SERVICES':
+        msg = 'Thank you for your interest in the ISI daily bible verse service! You are now unsubscribed to all services. Text "DAILY-SMS" anytime to resume services.'
+    else:
+        msg = """Thank you for your interest in the ISI daily bible verse service. The keyword you sent is not among the available options. Please choose from the following: "DAILY-SMS" : daily SMS subscription at 7am., "STOP-SERVICES" : unsubscribe from all services."""
 
-    return jsonify({'status': status, 'phone_number': phone_number})
+    if status in ('DAILY-SMS', 'STOP-SERVICES'):
+        #dynamodb_client.put_item(TableName=USERS_TABLE, Item={'phone_number': {'S': phone_number}, 'status': {'S': status}})
+        response = dynamodb_client.update_item(TableName=USERS_TABLE,
+            Key={'phone_number': phone_number},
+            UpdateExpression="set info.status=:s",
+            ExpressionAttributeValues={
+                ':s': status},
+            ReturnValues="UPDATED_NEW")
+
+
+        # dynamodb_client.update_item(TableName=USERS_TABLE, 
+        #                             Key={
+        #                                 'phone_number': {'S': phone_number}
+        #                             }, 
+        #                             AttributeUpdates={
+        #                                 'status': {
+        #                                     'Value': {'S': status},
+        #                                     'Action': 'PUT'
+        #                                 }
+        #                             }
+        # )
+    #create response
+    resp = MessagingResponse()
+    resp.message(msg)
+
+    return str(resp), 200, {'Content-Type': 'application/xml'}
 
 
 @app.errorhandler(404)
