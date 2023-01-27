5006e75f08ea4ba041eb8e898a04d2c49cfa2c52
Caleb M. Cook <calebmcook@icloud.com>
2023-01-26 18:42:21 -0700

    update dynamodb logic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 from flask import Flask, jsonify, make_response, request, abort
 from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
@@ -18,8 +20,6 @@ app = Flask(__name__)
 USERS_TABLE = os.environ['USERS_TABLE']
 TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 
-dynamodb_client = boto3.client('dynamodb')
-
 @app.route('/users', methods=['POST'])
 def create_user():
     #validate that the request originates from Twilio
@@ -37,29 +37,28 @@ def create_user():
     elif status == 'STOP-SERVICES':
         msg = 'Thank you for your interest in the ISI daily bible verse service! You are now unsubscribed to all services. Text "DAILY-SMS" anytime to resume services.'
     else:
-        msg = """Thank you for your interest in the ISI daily bible verse service. The keyword you sent is not among the available options. Please choose from the following: "DAILY-SMS" : daily SMS subscription at 7am., "STOP-SERVICES" : unsubscribe from all services."""
+        msg = 'Thank you for your interest in the ISI daily bible verse service. The keyword you sent is not among the available options. Please choose from the following: "DAILY-SMS" : daily SMS subscription at 7am., "STOP-SERVICES" : unsubscribe from all services.'
 
     if status in ('DAILY-SMS', 'STOP-SERVICES'):
-        #dynamodb_client.put_item(TableName=USERS_TABLE, Item={'phone_number': {'S': phone_number}, 'status': {'S': status}})
-        response = dynamodb_client.update_item(TableName=USERS_TABLE,
-            Key={'phone_number': phone_number},
-            UpdateExpression="set info.status=:s",
-            ExpressionAttributeValues={
-                ':s': status},
-            ReturnValues="UPDATED_NEW")
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(USERS_TABLE)
 
-
-        # dynamodb_client.update_item(TableName=USERS_TABLE, 
-        #                             Key={
-        #                                 'phone_number': {'S': phone_number}
-        #                             }, 
-        #                             AttributeUpdates={
-        #                                 'status': {
-        #                                     'Value': {'S': status},
-        #                                     'Action': 'PUT'
-        #                                 }
-        #                             }
-        # )
+        try:
+            response = table.update_item(
+                        Key={'phone_number': phone_number},
+                        UpdateExpression="set current_status = :s",
+                        ExpressionAttributeValues={
+                            ':s': current_status},
+                        ReturnValues="UPDATED_NEW")
+        except ClientError as err:
+            logger.error(
+                        "Couldn't update item %s in table %s. Here's why: %s: %s",
+                        phone_number, table.name,
+                        err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            print(response['Attributes'])
+        
     #create response
     resp = MessagingResponse()
     resp.message(msg)
